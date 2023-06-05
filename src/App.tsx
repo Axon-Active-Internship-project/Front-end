4dda0bedb63682270494f04c3a229fd3f1a0ed4d
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-06-05 15:59:39 +0700

    Add reacr-router-dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Router } from "./utils";
+import { BrowserRouter } from "react-router-dom";
 
-const  App = () => {
-  return <>
-    
-  </>;
-}
+const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <>
+      <ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
