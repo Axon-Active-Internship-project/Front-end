9a2e3033d1542e36d13981cad816c0695ad29e9b
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-06-13 15:49:30 +0700

    Import error and not found page

--- src/utils/Routers.tsx
+++ src/utils/Routers.tsx
@@ -1,12 +1,18 @@
 import { useRoutes } from "react-router-dom";
 import { MainLayuot } from "../layouts";
 import { RouteObject } from "react-router";
+import { NotFound, Error } from "../pages/";
 
 const Router = () => {
   const router: RouteObject[] = [
     {
       path: "",
       element: <MainLayuot />,
+      errorElement: <Error />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
     },
   ];
 
