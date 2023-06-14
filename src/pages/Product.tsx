d8acfe5379168da36d927aef4a71ffed03a4aed9
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-06-14 13:43:34 +0700

    Mock Render card component

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,16 +1,21 @@
-import { Grid } from "@chakra-ui/react";
-import { IProduct } from "../interfaces";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { ProductProps } from "../interfaces";
 import { Filter, Card } from "../components";
 
-interface ProductProps {
-  data: IProduct[];
-}
 const Product = ({ data }: ProductProps) => {
   console.log(data);
   return (
-    <Grid>
-      <Filter></Filter>
-      <Card name="asd" price="1000" id={1} sale_price="900" />
+    <Grid gap={12} templateColumns={"repeat(4, 1fr)"}>
+      <GridItem>
+        <Filter></Filter>
+      </GridItem>
+      <GridItem colStart={2} colSpan={3}>
+        <Grid templateColumns={"repeat(3, 1fr)"} gap={"30px"}>
+          {data.map((item) => (
+            <Card data={item} key={item.id} />
+          ))}
+        </Grid>
+      </GridItem>
     </Grid>
   );
 };
