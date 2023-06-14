ea77a759da8738a61d499cb4a1f63f768f22f191
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-06-14 13:40:53 +0700

    Ui/ Update card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,35 +6,78 @@ import {
   Image,
   Text,
   HStack,
+  Button,
+  Flex,
+  Icon,
 } from "@chakra-ui/react";
-import IProduct from "../../interfaces/product";
+import { CardProps } from "../../interfaces/product";
+import { FaCartPlus, FaPlus } from "react-icons/fa";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const MyCard = ({ ...data }: IProduct) => {
+const MyCard = ({ data }: CardProps) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   const { name, price, sale_price, src } = data;
   return (
-    <Card w={330} h={355}>
-      <CardBody>
-        <Image
-          w={"100%"}
-          h={"300"}
-          src={
-            "https://s1.storage.5giay.vn/image/2016/10/20161021_b84b6337cc54488264598cf7e498eeff_1477033027.jpg"
-          }
-        />
-        <Box>
-          <Heading>{name}</Heading>
-          <Box>
-            {!sale_price ? (
-              <Text>{price} đ</Text>
-            ) : (
-              <HStack spacing={10}>
-                <Text>{sale_price} đ</Text>
-                <Text as="s">{price} đ</Text>
-              </HStack>
-            )}
+    <Card
+      maxW={330}
+      maxH={355}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <Link to={"/"}>
+        <CardBody padding={1}>
+          <Image
+            w={"100%"}
+            h={"300"}
+            src={src}
+            borderRadius={10}
+            objectFit={"fill"}
+            opacity={isHovering ? 0.5 : 1}
+          />
+          <Box mx={4} pt={1}>
+            <Heading as={"h5"} size={"md"} textTransform={"capitalize"}>
+              {name}
+            </Heading>
+            <Box>
+              {!sale_price ? (
+                <Text>{price} đ</Text>
+              ) : (
+                <HStack spacing={5}>
+                  <Text>{sale_price} đ</Text>
+                  <Text as="s">{price} đ</Text>
+                </HStack>
+              )}
+            </Box>
           </Box>
-        </Box>
-      </CardBody>
+        </CardBody>
+      </Link>
+      {isHovering && (
+        <Flex
+          gap={5}
+          justifyContent={"center"}
+          pos={"absolute"}
+          bottom={55}
+          right={0}
+          left={0}
+        >
+          <Button colorScheme="yellow">
+            <Icon as={FaPlus} boxSize={"5"} />
+          </Button>
+          <Button colorScheme="green">
+            <Icon as={FaCartPlus} boxSize={"5"} />
+          </Button>
+        </Flex>
+      )}
     </Card>
   );
 };
