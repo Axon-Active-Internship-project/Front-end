56bf05e52744b6eb665e6d0d42c0bb83db42b0cd
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-06-16 14:45:36 +0700

    Implement filter component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { FilterProps } from "../../interfaces";
+import { useState } from "react";
+import {
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Heading,
+  Flex,
+  HStack,
+  Text,
+  Kbd,
+} from "@chakra-ui/react";
 
-const Filter = () => {
-  return <div>Filter</div>;
+const Filter = ({
+  minValue,
+  maxValue,
+  step = 5,
+  onHandleFilter,
+}: FilterProps) => {
+  const [sliderValue, setSliderValue] = useState([minValue, maxValue]);
+
+  return (
+    <Flex
+      flexDirection={"column"}
+      gap={4}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Heading as={"h3"} fontSize={22}>
+        Fillter By Price
+      </Heading>
+      <RangeSlider
+        defaultValue={[minValue, maxValue]}
+        min={minValue}
+        max={maxValue}
+        step={step}
+        onChange={(v) => {
+          onHandleFilter(v[0], v[1]);
+          setSliderValue(v);
+        }}
+      >
+        <RangeSliderTrack bg="red.100" boxSize={1.5} borderRadius={15}>
+          <RangeSliderFilledTrack bg="tomato" />
+        </RangeSliderTrack>
+        <RangeSliderThumb boxSize={5} index={0} />
+        <RangeSliderThumb boxSize={5} index={1} />
+      </RangeSlider>
+      <HStack spacing={4}>
+        <Kbd fontSize={16}>{sliderValue[0]} đ</Kbd>
+        <Text>-</Text>
+        <Kbd fontSize={16}> {sliderValue[1]} đ</Kbd>
+      </HStack>
+    </Flex>
+  );
 };
 
 export default Filter;
