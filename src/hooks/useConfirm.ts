ad64f253c258e4f4c0f1cd48bc9391d67230a998
HuyND11 <huy.nguyen23@student.passerellesnumeriques.org>
2023-07-12 15:29:20 +0700

    Update focus on cancel button

--- src/hooks/useConfirm.ts
+++ src/hooks/useConfirm.ts
@@ -14,6 +14,7 @@ const useConfirm = () => {
       showCancelButton: true,
       cancelButtonText: "Cancel",
       cancelButtonColor: "red",
+      focusCancel: true,
     });
 
     return isConfirmed;
